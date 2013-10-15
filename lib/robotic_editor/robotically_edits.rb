4c3c32396d1e6dc3d70463c1fcfe511758d9d5b6
Daniel Staudigel <dstaudigel@gmail.com>
2013-10-15 16:56:45 -0700

    Adding iframe support.

--- lib/robotic_editor/robotically_edits.rb
+++ lib/robotic_editor/robotically_edits.rb
@@ -1,5 +1,5 @@
 require 'robotic_editor/robotically_edits/adapter'
-
+ 
 module RoboticEditor
   module RoboticallyEdits
     def self.noko_parentify node
@@ -10,7 +10,7 @@ module RoboticEditor
       after_node = parent.clone
       # empty it out:
       after_node.children.each { |c| c.remove }
-      
+
       # move every sibling after <node> in to the <after_node>
       while sib = node.next_sibling do
         sib.remove
@@ -20,10 +20,10 @@ module RoboticEditor
 
       # now remove <node>
       node.remove
-      
+
       # and add it after the thing that used to be our parent:
       parent.add_next_sibling node
-      
+
       # and insert our next thing:
       node.add_next_sibling after_node
 
@@ -31,40 +31,64 @@ module RoboticEditor
       parent.remove if parent.text.length == 0
     end
 
+    def self.proc_img img , i
+      # drop any style:
+      style = img.attr(:style);
+      img.set_attribute(:style,'');
+
+      # make every image top-level
+      while img.parent.name == "p" do
+        noko_parentify img
+      end
+
+      src = img.attr(:src)
+
+      id = /.*\/(.*?)\/.*?$/.match(src)[1] rescue nil
+      picture = RedactorRails::Picture.find(id)
+
+      woverh = picture.width.to_f / picture.height.to_f
+      if woverh > 1.2
+        partial = "shared/redactor_image_landscape" 
+      elsif woverh < 0.8
+        partial = "shared/redactor_image_portrait"
+      else
+        partial = "shared/redactor_image_square" # could be made "square" in future
+      end
+
+      multi_view = render_partial partial , { :image => picture, :woverh => woverh, :index => i } 
+
+      img.replace(multi_view)
+    end
+    
+    def self.proc_iframe(iframe,index)
+      width = iframe.attr(:width).to_f
+      height = iframe.attr(:height).to_f
+      woverh = width/height
+
+      if woverh > 1.2
+        partial = "robotic_editor/iframe_landscape" 
+      elsif woverh < 0.8
+        partial = "robotic_editor/iframe_portrait"
+      else
+        partial = "robotic_editor/iframe_landscape" # could be made "square" in future
+      end
+
+      iframe.replace(render_partial(partial,{ :content => iframe.to_s , :woverh => woverh , :index => index }))
+    end
+
+    def self.render_partial(partial , locals)
+      ApplicationController.new.render_to_string( :partial => partial ,
+                                                  :locals => locals )
+    end
+
     def self.edit_robotically(the_content)
       doc = Nokogiri::HTML(the_content)
-      doc.css('img').each_with_index do |img,i|
-        # drop any style:
-        style = img.attr(:style);
-        img.set_attribute(:style,'');
-        
-        # make every image top-level
-        while img.parent.name == "p" do
-          noko_parentify img
+      doc.css('img,iframe').each_with_index do |tag,i|
+        if tag.name == 'iframe'
+          proc_iframe tag,i
+        elsif tag.name == 'img'
+          proc_img tag,i
         end
-
-        src = img.attr(:src)
-
-        id = /.*\/(.*?)\/.*?$/.match(src)[1] rescue nil
-        picture = RedactorRails::Picture.find(id)
-
-        woverh = picture.width.to_f / picture.height.to_f
-        if woverh > 1.2
-          partial = "shared/redactor_image_landscape" 
-        elsif woverh < 0.8
-          partial = "shared/redactor_image_portrait"
-        else
-          partial = "shared/redactor_image_square" # could be made "square" in future
-        end
-        puts "INDEX: #{i} , #{ (i % 2==0) ? 'even' : 'odd' }"
-        multi_view = ApplicationController.new.render_to_string( :partial => partial , 
-                                                                 :locals => { 
-                                                                   :image => picture, 
-                                                                   :woverh => woverh,
-                                                                   :index => i
-                                                                 } )
-
-        img.replace(multi_view)
       end
       # <!DOCTYPE><html><body><-- what we're interested in --></body></html>
       # hence children[1],children[0].children:
